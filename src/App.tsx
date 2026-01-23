4c5afb19867631f9821aacdd1bea444cfa686dbc
Özgür Karaaslan <ozgurkaraaslan351@gmail.com>
2026-01-23 20:27:01 +0300

    Building Game Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./maincomponents/NavBar";
---
+++ src/maincomponents/GameCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Center, Heading, Image } from "@chakra-ui/react";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={20} overflow={"hidden"}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize={20} textAlign={"center"}>
+          {game.name}
+        </Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
