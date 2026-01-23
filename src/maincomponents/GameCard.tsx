43cb60d006622edc33cd47fe08697722578b0f92
Özgür Karaaslan <ozgurkaraaslan351@gmail.com>
2026-01-23 22:42:52 +0300

    Displaying platform icons and make a new design for game cards

--- src/maincomponents/GameCard.tsx
+++ src/maincomponents/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Center, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Center, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -8,12 +9,13 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={20} overflow={"hidden"} height={"300px"}>
+    <Card borderRadius={20} overflow={"hidden"}>
       <Image src={game.background_image} height={"233px"} objectFit={"cover"} />
       <CardBody>
-        <Heading fontSize={20} textAlign={"center"}>
-          {game.name}
-        </Heading>
+        <Heading fontSize={27}>{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
