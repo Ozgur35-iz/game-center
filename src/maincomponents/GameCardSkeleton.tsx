b3e54e2877582444b030343693888a8ce573730d
Özgür Karaaslan <ozgurkaraaslan351@gmail.com>
2026-01-25 23:06:01 +0300

    Show loading skeletons

---
+++ src/maincomponents/GameCardSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card
+      height={"427px"}
+      width={"320px"}
+      borderRadius={20}
+      overflow={"hidden"}
+    >
+      <Skeleton />
+      <CardBody>
+        <SkeletonText></SkeletonText>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
--- src/maincomponents/GameGrid.tsx
+++ src/maincomponents/GameGrid.tsx
@@ -1,6 +1,7 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Game {
   id: number;
@@ -13,7 +14,9 @@ interface FetchGamesResponse {
 }
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       {error && <Text>Error: {error}</Text>}
@@ -22,6 +25,8 @@ const GameGrid = () => {
         spacing={10}
         padding={10}
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
