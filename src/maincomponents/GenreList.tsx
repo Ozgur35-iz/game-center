eda92572cae6eb892fb2192c76a1d314f778b0bb
Özgür Karaaslan <ozgurkaraaslan351@gmail.com>
2026-01-30 17:26:20 +0300

    Clean up the genres

--- src/maincomponents/GenreList.tsx
+++ src/maincomponents/GenreList.tsx
@@ -1,5 +1,7 @@
 import {
+  Box,
   Button,
+  Heading,
   HStack,
   Image,
   Link,
@@ -10,6 +12,7 @@ import {
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import { m } from "framer-motion";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -21,31 +24,38 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   if (isLoading) return <Spinner />;
   return (
-    <List>
-      {genres.map((genre) => (
-        <ListItem key={genre.id}>
-          <Button
-            fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-            onClick={() => {
-              onSelectGenre(genre);
-            }}
-            variant={"link"}
-          >
-            <HStack>
-              <Image
-                src={getCroppedImageUrl(genre.image_background)}
-                boxSize={"42px"}
-                borderRadius={8}
-                objectFit={"cover"}
-              ></Image>
-              <Text fontSize={"lg"} paddingLeft={"4px"}>
-                {genre.name}
-              </Text>
-            </HStack>
-          </Button>
-        </ListItem>
-      ))}
-    </List>
+    <Box>
+      <Heading fontSize={"27px"} marginBottom={"5px"}>
+        Genres
+      </Heading>
+      <List>
+        {genres.map((genre) => (
+          <ListItem key={genre.id}>
+            <Button
+              whiteSpace={"normal"}
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              onClick={() => {
+                onSelectGenre(genre);
+              }}
+              variant={"link"}
+              textAlign={"left"}
+            >
+              <HStack>
+                <Image
+                  src={getCroppedImageUrl(genre.image_background)}
+                  boxSize={"42px"}
+                  borderRadius={8}
+                  objectFit={"cover"}
+                ></Image>
+                <Text fontSize={"lg"} paddingLeft={"4px"}>
+                  {genre.name}
+                </Text>
+              </HStack>
+            </Button>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 };
 
